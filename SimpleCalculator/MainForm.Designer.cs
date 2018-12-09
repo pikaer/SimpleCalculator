321d32333f331f26161fcb744ef1ef07ea2015c9
易林军 <1441457008@qq.com>
2018-12-09 18:57:44 +0800

    计算器制作

---
+++ SimpleCalculator/MainForm.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace SimpleCalculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNum7 = new System.Windows.Forms.Button();
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.btnNum8 = new System.Windows.Forms.Button();
+            this.btnNum9 = new System.Windows.Forms.Button();
+            this.btnChu = new System.Windows.Forms.Button();
+            this.btnCE = new System.Windows.Forms.Button();
+            this.btnNum4 = new System.Windows.Forms.Button();
+            this.btnNum1 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnDeng = new System.Windows.Forms.Button();
+            this.btnCheng = new System.Windows.Forms.Button();
+            this.btnNum6 = new System.Windows.Forms.Button();
+            this.btnNum5 = new System.Windows.Forms.Button();
+            this.btnJian = new System.Windows.Forms.Button();
+            this.btnNum3 = new System.Windows.Forms.Button();
+            this.btnNum2 = new System.Windows.Forms.Button();
+            this.btnJia = new System.Windows.Forms.Button();
+            this.btnPoint = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnJia);
+            this.groupBox1.Controls.Add(this.btnPoint);
+            this.groupBox1.Controls.Add(this.btnJian);
+            this.groupBox1.Controls.Add(this.btnNum3);
+            this.groupBox1.Controls.Add(this.btnNum2);
+            this.groupBox1.Controls.Add(this.btnCheng);
+            this.groupBox1.Controls.Add(this.btnNum6);
+            this.groupBox1.Controls.Add(this.btnNum5);
+            this.groupBox1.Controls.Add(this.btnDeng);
+            this.groupBox1.Controls.Add(this.btn0);
+            this.groupBox1.Controls.Add(this.btnNum1);
+            this.groupBox1.Controls.Add(this.btnNum4);
+            this.groupBox1.Controls.Add(this.btnCE);
+            this.groupBox1.Controls.Add(this.btnChu);
+            this.groupBox1.Controls.Add(this.btnNum9);
+            this.groupBox1.Controls.Add(this.btnNum8);
+            this.groupBox1.Controls.Add(this.btnNum7);
+            this.groupBox1.Controls.Add(this.txtShow);
+            this.groupBox1.Location = new System.Drawing.Point(36, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(783, 568);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "主界面";
+            // 
+            // btnNum7
+            // 
+            this.btnNum7.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum7.Location = new System.Drawing.Point(32, 126);
+            this.btnNum7.Name = "btnNum7";
+            this.btnNum7.Size = new System.Drawing.Size(105, 60);
+            this.btnNum7.TabIndex = 1;
+            this.btnNum7.Text = "7";
+            this.btnNum7.UseVisualStyleBackColor = true;
+            this.btnNum7.Click += new System.EventHandler(this.btnNum7_Click);
+            // 
+            // txtShow
+            // 
+            this.txtShow.BackColor = System.Drawing.SystemColors.Window;
+            this.txtShow.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtShow.Location = new System.Drawing.Point(32, 42);
+            this.txtShow.Name = "txtShow";
+            this.txtShow.Size = new System.Drawing.Size(713, 50);
+            this.txtShow.TabIndex = 0;
+            // 
+            // btnNum8
+            // 
+            this.btnNum8.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum8.Location = new System.Drawing.Point(180, 126);
+            this.btnNum8.Name = "btnNum8";
+            this.btnNum8.Size = new System.Drawing.Size(105, 60);
+            this.btnNum8.TabIndex = 2;
+            this.btnNum8.Text = "8";
+            this.btnNum8.UseVisualStyleBackColor = true;
+            this.btnNum8.Click += new System.EventHandler(this.btnNum8_Click);
+            // 
+            // btnNum9
+            // 
+            this.btnNum9.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum9.Location = new System.Drawing.Point(328, 126);
+            this.btnNum9.Name = "btnNum9";
+            this.btnNum9.Size = new System.Drawing.Size(105, 60);
+            this.btnNum9.TabIndex = 3;
+            this.btnNum9.Text = "9";
+            this.btnNum9.UseVisualStyleBackColor = true;
+            this.btnNum9.Click += new System.EventHandler(this.btnNum9_Click);
+            // 
+            // btnChu
+            // 
+            this.btnChu.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnChu.Location = new System.Drawing.Point(476, 126);
+            this.btnChu.Name = "btnChu";
+            this.btnChu.Size = new System.Drawing.Size(105, 60);
+            this.btnChu.TabIndex = 4;
+            this.btnChu.Text = "/";
+            this.btnChu.UseVisualStyleBackColor = true;
+            this.btnChu.Click += new System.EventHandler(this.btnChu_Click);
+            // 
+            // btnCE
+            // 
+            this.btnCE.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCE.Location = new System.Drawing.Point(624, 126);
+            this.btnCE.Name = "btnCE";
+            this.btnCE.Size = new System.Drawing.Size(105, 60);
+            this.btnCE.TabIndex = 5;
+            this.btnCE.Text = "CE";
+            this.btnCE.UseVisualStyleBackColor = true;
+            this.btnCE.Click += new System.EventHandler(this.btnCE_Click);
+            // 
+            // btnNum4
+            // 
+            this.btnNum4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum4.Location = new System.Drawing.Point(32, 244);
+            this.btnNum4.Name = "btnNum4";
+            this.btnNum4.Size = new System.Drawing.Size(105, 60);
+            this.btnNum4.TabIndex = 6;
+            this.btnNum4.Text = "4";
+            this.btnNum4.UseVisualStyleBackColor = true;
+            this.btnNum4.Click += new System.EventHandler(this.btnNum4_Click);
+            // 
+            // btnNum1
+            // 
+            this.btnNum1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum1.Location = new System.Drawing.Point(32, 362);
+            this.btnNum1.Name = "btnNum1";
+            this.btnNum1.Size = new System.Drawing.Size(105, 60);
+            this.btnNum1.TabIndex = 7;
+            this.btnNum1.Text = "1";
+            this.btnNum1.UseVisualStyleBackColor = true;
+            this.btnNum1.Click += new System.EventHandler(this.btnNum1_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn0.Location = new System.Drawing.Point(32, 480);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(253, 60);
+            this.btn0.TabIndex = 8;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnDeng
+            // 
+            this.btnDeng.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnDeng.Location = new System.Drawing.Point(624, 244);
+            this.btnDeng.Name = "btnDeng";
+            this.btnDeng.Size = new System.Drawing.Size(105, 296);
+            this.btnDeng.TabIndex = 9;
+            this.btnDeng.Text = "=";
+            this.btnDeng.UseVisualStyleBackColor = true;
+            this.btnDeng.Click += new System.EventHandler(this.btnDeng_Click);
+            // 
+            // btnCheng
+            // 
+            this.btnCheng.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCheng.Location = new System.Drawing.Point(476, 244);
+            this.btnCheng.Name = "btnCheng";
+            this.btnCheng.Size = new System.Drawing.Size(105, 60);
+            this.btnCheng.TabIndex = 12;
+            this.btnCheng.Text = "*";
+            this.btnCheng.UseVisualStyleBackColor = true;
+            this.btnCheng.Click += new System.EventHandler(this.btnCheng_Click);
+            // 
+            // btnNum6
+            // 
+            this.btnNum6.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum6.Location = new System.Drawing.Point(328, 244);
+            this.btnNum6.Name = "btnNum6";
+            this.btnNum6.Size = new System.Drawing.Size(105, 60);
+            this.btnNum6.TabIndex = 11;
+            this.btnNum6.Text = "6";
+            this.btnNum6.UseVisualStyleBackColor = true;
+            this.btnNum6.Click += new System.EventHandler(this.btnNum6_Click);
+            // 
+            // btnNum5
+            // 
+            this.btnNum5.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum5.Location = new System.Drawing.Point(180, 244);
+            this.btnNum5.Name = "btnNum5";
+            this.btnNum5.Size = new System.Drawing.Size(105, 60);
+            this.btnNum5.TabIndex = 10;
+            this.btnNum5.Text = "5";
+            this.btnNum5.UseVisualStyleBackColor = true;
+            this.btnNum5.Click += new System.EventHandler(this.btnNum5_Click);
+            // 
+            // btnJian
+            // 
+            this.btnJian.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnJian.Location = new System.Drawing.Point(476, 362);
+            this.btnJian.Name = "btnJian";
+            this.btnJian.Size = new System.Drawing.Size(105, 60);
+            this.btnJian.TabIndex = 15;
+            this.btnJian.Text = "-";
+            this.btnJian.UseVisualStyleBackColor = true;
+            this.btnJian.Click += new System.EventHandler(this.btnJian_Click);
+            // 
+            // btnNum3
+            // 
+            this.btnNum3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum3.Location = new System.Drawing.Point(328, 362);
+            this.btnNum3.Name = "btnNum3";
+            this.btnNum3.Size = new System.Drawing.Size(105, 60);
+            this.btnNum3.TabIndex = 14;
+            this.btnNum3.Text = "3";
+            this.btnNum3.UseVisualStyleBackColor = true;
+            this.btnNum3.Click += new System.EventHandler(this.btnNum3_Click);
+            // 
+            // btnNum2
+            // 
+            this.btnNum2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum2.Location = new System.Drawing.Point(180, 362);
+            this.btnNum2.Name = "btnNum2";
+            this.btnNum2.Size = new System.Drawing.Size(105, 60);
+            this.btnNum2.TabIndex = 13;
+            this.btnNum2.Text = "2";
+            this.btnNum2.UseVisualStyleBackColor = true;
+            this.btnNum2.Click += new System.EventHandler(this.btnNum2_Click);
+            // 
+            // btnJia
+            // 
+            this.btnJia.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnJia.Location = new System.Drawing.Point(476, 480);
+            this.btnJia.Name = "btnJia";
+            this.btnJia.Size = new System.Drawing.Size(105, 60);
+            this.btnJia.TabIndex = 17;
+            this.btnJia.Text = "+";
+            this.btnJia.UseVisualStyleBackColor = true;
+            this.btnJia.Click += new System.EventHandler(this.btnJia_Click);
+            // 
+            // btnPoint
+            // 
+            this.btnPoint.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnPoint.Location = new System.Drawing.Point(328, 480);
+            this.btnPoint.Name = "btnPoint";
+            this.btnPoint.Size = new System.Drawing.Size(105, 60);
+            this.btnPoint.TabIndex = 16;
+            this.btnPoint.Text = ".";
+            this.btnPoint.UseVisualStyleBackColor = true;
+            this.btnPoint.Click += new System.EventHandler(this.btnPoint_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 646);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(882, 717);
+            this.MinimumSize = new System.Drawing.Size(882, 717);
+            this.Name = "MainForm";
+            this.Text = "简易计算器";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnNum7;
+        private System.Windows.Forms.TextBox txtShow;
+        private System.Windows.Forms.Button btnJia;
+        private System.Windows.Forms.Button btnPoint;
+        private System.Windows.Forms.Button btnJian;
+        private System.Windows.Forms.Button btnNum3;
+        private System.Windows.Forms.Button btnNum2;
+        private System.Windows.Forms.Button btnCheng;
+        private System.Windows.Forms.Button btnNum6;
+        private System.Windows.Forms.Button btnNum5;
+        private System.Windows.Forms.Button btnDeng;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnNum1;
+        private System.Windows.Forms.Button btnNum4;
+        private System.Windows.Forms.Button btnCE;
+        private System.Windows.Forms.Button btnChu;
+        private System.Windows.Forms.Button btnNum9;
+        private System.Windows.Forms.Button btnNum8;
+    }
+}
+
---
+++ SimpleCalculator/MainForm.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Windows.Forms;
+
+/// <summary>
+/// 简易计算器
+/// </summary>
+namespace SimpleCalculator
+{
+    public partial class MainForm : Form
+    {
+        //当前操作键
+        CurrentOperation currentOperation;
+
+        //第一次按下的值
+        string firstNumTxt;
+
+        //第二次按下的值
+        string secondNumTxt;
+
+        //第一次按下的值是否可清除
+        bool canClearfirstNum ;
+
+        /// <summary>
+        /// 初始化值
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        /// <summary>
+        /// 计算
+        /// </summary>
+        public void Calculator()
+        {
+
+            if (currentOperation == CurrentOperation.Default|| secondNumTxt=="")
+            {
+                return;
+            }
+
+            string txtShowValue = "";
+            double firstNum = Convert.ToDouble(firstNumTxt);
+            double secondNum = Convert.ToDouble(secondNumTxt);
+
+            switch (currentOperation)
+            {
+                case CurrentOperation.Jia:
+                    txtShowValue = (firstNum + secondNum).ToString();
+                    break;
+                case CurrentOperation.Jian:
+                    txtShowValue = (firstNum - secondNum).ToString();
+                    break;
+                case CurrentOperation.Cheng:
+                    txtShowValue = (firstNum * secondNum).ToString();
+                    break;
+                case CurrentOperation.Chu:
+                    if (secondNum != 0)
+                    {
+                        txtShowValue = (firstNum / secondNum).ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("除数不能为0！");
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            txtShow.Text = txtShowValue;
+
+            //操作键初始化
+            firstNumTxt = txtShowValue;
+            secondNumTxt = "";
+            currentOperation = CurrentOperation.Default;
+            canClearfirstNum = true;
+        }
+        
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void Init()
+        {
+            firstNumTxt = "";
+            secondNumTxt = "";
+            currentOperation = CurrentOperation.Default;
+            txtShow.Text = "";
+        }
+
+        /// <summary>
+        /// 按下数字按钮后追加到文本末尾
+        /// </summary>
+        /// <param name="num">数字键</param>
+        public void SetNum(CurrentNum num)
+        {
+            string txt;
+
+            //如果当前没有按加减乘除键，即是将数字追加到第一个值
+            if (currentOperation == CurrentOperation.Default)
+            {
+                if(canClearfirstNum)
+                {
+                    firstNumTxt = "";
+                    canClearfirstNum = false;
+                }
+
+                txt = SetNumValue(num, firstNumTxt.ToString());
+                firstNumTxt = txt;
+            }
+            else
+            {
+                txt = SetNumValue(num, secondNumTxt.ToString());
+                secondNumTxt = txt;
+            }
+            txtShow.Text = txt;
+        }
+
+        /// <summary>
+        /// 追加到文本末尾
+        /// </summary>
+        /// <param name="num">数字键</param>
+        /// <param name="txt">当前文本值</param>
+        /// <returns></returns>
+        public string SetNumValue(CurrentNum num, string txt)
+        {
+            if ((txt == "0" && num == CurrentNum.Num0) ||
+                (txt == "" && num == CurrentNum.Point) ||
+                (txt.Contains(".") && num == CurrentNum.Point))
+            {
+                return txt;
+            }
+
+            if (num == CurrentNum.Point)
+            {
+                txt += ".";
+            }
+            else
+            {
+                //将数字键对应的值追加到文本上
+                txt += (int)num;
+            }
+
+            if (txt!= "0" && txt != "0." &&Convert.ToDouble(txt) >1)
+            {
+                txt= txt.TrimStart('0').ToString();
+            }
+            return txt;
+        }
+
+        #region 按钮点击事件
+
+        #region 数字按钮单击事件
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            SetNum(CurrentNum.Num0);
+        }
+
+        private void btnNum1_Click(object sender, EventArgs e)
+        {
+            SetNum(CurrentNum.Num1);
+        }
+
+        private void btnNum2_Click(object sender, EventArgs e)
+        {
+            SetNum(CurrentNum.Num2);
+        }
+
+        private void btnNum3_Click(object sender, EventArgs e)
+        {
+            SetNum(CurrentNum.Num3);
+        }
+
+        private void btnNum4_Click(object sender, EventArgs e)
+        {
+            SetNum(CurrentNum.Num4);
+        }
+
+        private void btnNum5_Click(object sender, EventArgs e)
+        {
+            SetNum(CurrentNum.Num5);
+        }
+
+        private void btnNum6_Click(object sender, EventArgs e)
+        {
+            SetNum(CurrentNum.Num6);
+        }
+
+        private void btnNum7_Click(object sender, EventArgs e)
+        {
+            SetNum(CurrentNum.Num7);
+        }
+
+        private void btnNum8_Click(object sender, EventArgs e)
+        {
+            SetNum(CurrentNum.Num8);
+        }
+
+        private void btnNum9_Click(object sender, EventArgs e)
+        {
+            SetNum(CurrentNum.Num9);
+        }
+
+        private void btnPoint_Click(object sender, EventArgs e)
+        {
+            SetNum(CurrentNum.Point);
+        }
+        #endregion
+
+        #region 操作按钮单机事件
+
+        private void btnJia_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtShow.Text))
+            {
+                currentOperation = CurrentOperation.Jia;
+            }
+        }
+
+        private void btnJian_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtShow.Text))
+            {
+                currentOperation = CurrentOperation.Jian;
+            }
+        }
+
+        private void btnCheng_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtShow.Text))
+            {
+                currentOperation = CurrentOperation.Cheng;
+            }
+        }
+
+        private void btnChu_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtShow.Text))
+            {
+                currentOperation = CurrentOperation.Chu;
+            }
+        }
+
+        /// <summary>
+        /// 归零(初始化）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCE_Click(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void btnDeng_Click(object sender, EventArgs e)
+        {
+            Calculator();
+        }
+        #endregion
+
+        #endregion
+    }
+
+    /// <summary>
+    /// 当前操作枚举
+    /// </summary>
+    public enum CurrentOperation
+    {
+        /// <summary>
+        /// 加(+)
+        /// </summary>
+        Jia=1,
+
+        /// <summary>
+        /// 减(-)
+        /// </summary>
+        Jian=2,
+
+        /// <summary>
+        /// 乘(*)
+        /// </summary>
+        Cheng=3,
+
+        /// <summary>
+        /// 除(/)
+        /// </summary>
+        Chu=4,
+        
+        /// <summary>
+        /// 默认初始状态
+        /// </summary>
+        Default=5
+    }
+
+    /// <summary>
+    /// 当前按下的数字键枚举
+    /// </summary>
+    public enum CurrentNum
+    {
+        /// <summary>
+        /// 数字0
+        /// </summary>
+        Num0=0,
+
+        /// <summary>
+        /// 数字1
+        /// </summary>
+        Num1=1,
+
+        /// <summary>
+        /// 数字2
+        /// </summary>
+        Num2=2,
+
+        /// <summary>
+        /// 数字3
+        /// </summary>
+        Num3=3,
+
+        /// <summary>
+        /// 数字4
+        /// </summary>
+        Num4=4,
+
+        /// <summary>
+        /// 数字5
+        /// </summary>
+        Num5=5,
+
+        /// <summary>
+        /// 数字6
+        /// </summary>
+        Num6=6,
+
+        /// <summary>
+        /// 数字7
+        /// </summary>
+        Num7=7,
+
+        /// <summary>
+        /// 数字8
+        /// </summary>
+        Num8=8,
+
+        /// <summary>
+        /// 数字9
+        /// </summary>
+        Num9=9,
+
+        /// <summary>
+        /// 小数点
+        /// </summary>
+        Point=10,
+    }
+}
